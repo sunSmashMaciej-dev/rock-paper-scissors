fdfb8cbec0ebe2ca64945276e3abb8471cd3de42
sunSmashMaciej <117889378+sunSmashMaciej-dev@users.noreply.github.com>
2022-12-15 06:12:04 -0600

    Play one round of the game+declare winner

--- script.js
+++ script.js
@@ -2,29 +2,52 @@
 const playHand = ['rock', 'paper', 'scissors'];
 
 //Get player's choice:
-    //Prompt player for selection
-    //Store result
 function getPlayerChoice() {
-    let playerSelection = prompt('What will you choose: rock, paper or scissors?');
-    return(playerSelection.toLowerCase());
+    //Prompt player for selection
+    let playerChoice= prompt('What will you choose: rock, paper or scissors?');
+    //Store result
+    return(playerChoice.toLowerCase());
 }
-console.log(getPlayerChoice())
 
 //Get computer's choice:
-    //Randomize selection
-    //Store result
 function getComputerChoice() {
-    let computerSelection = playHand[Math.floor(Math.random() * playHand.length)];
-    return(computerSelection);
-}
-console.log(getComputerChoice());
-
-//Display player's round result
-    //"You win!!!" (how they won e.g. Paper beats Rock)
-        //else "You lose :(" (how they lost)
-        //else if tie "Round is a tie."
-    //Win gets 1 point, lose or tie gets none
+    //Randomize selection
+    let computerChoice = playHand[Math.floor(Math.random() * playHand.length)];
     //Store result
+    return(computerChoice);
+}
+
+//Declare parameters for playRound function
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    //Display player's round result:
+    //"You win!!!" (how they won e.g. Paper beats Rock)
+    if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        return('You win!!! Rock beats Scissors');
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return('Congratulations!!! Paper beats Rock');
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return('Boo-ya!!! Scissors beats Paper');
+    
+    //else "You lose :(" (how they lost)
+    } else if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return('You lose :( Paper beats Rock');
+    } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
+        return('Bad luck :\'( Scissors beats paper');
+    } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
+        return('Sorry -_- Rock beats Scissors');
+
+    //else if tie "Round is a tie."
+    } else if(playerSelection === computerSelection) {
+        return('Round is a tie...');
+    } else {
+        return('something went wrong');
+    }
+  
+}
+console.log(playRound(playerSelection, computerSelection));
 
 //Repeat round 5 times
 
