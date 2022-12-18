0f88ef46d88eadcc3efa2f01b8e683546a95380a
sunSmashMaciej <117889378+sunSmashMaciej-dev@users.noreply.github.com>
2022-12-18 02:51:15 -0600

    Create roundResult variable+implement in round

--- script.js
+++ script.js
@@ -18,45 +18,52 @@ function getComputerChoice() {
 }
 
 //Declare parameters for playRound function
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+let roundResult = '';
 
 function playRound(playerSelection, computerSelection) {
     //Display player's round result:
     //"You win!!!" (how they won e.g. Paper beats Rock)
     if(playerSelection === 'rock' && computerSelection === 'scissors') {
-        return('You win!!! Rock beats Scissors');
+        roundResult ='You win!!! Rock beats Scissors';
     } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-        return('Congratulations!!! Paper beats Rock');
+        roundResult ='Congratulations!!! Paper beats Rock';
     } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
-        return('Boo-ya!!! Scissors beats Paper');
+        roundResult ='Boo-ya!!! Scissors beats Paper';
     
     //else "You lose :(" (how they lost)
     } else if(playerSelection === 'rock' && computerSelection === 'paper') {
-        return('You lose :( Paper beats Rock');
+        roundResult ='You lose :( Paper beats Rock';
     } else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-        return('Bad luck :\'( Scissors beats paper');
+        roundResult ='Bad luck :\'( Scissors beats paper';
     } else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-        return('Sorry -_- Rock beats Scissors');
+        roundResult ='Sorry -_- Rock beats Scissors';
 
     //else if tie "Round is a tie."
     } else if(playerSelection === computerSelection) {
-        return('Round is a tie...');
+        roundResult ='Round is a tie...';
     } else {
-        return('something went wrong');
+        roundResult ='something went wrong';
     }
-  
+    console.log('You played ' + playerSelection);
+    console.log('The computer played ' + computerSelection);
+    return(roundResult);
 }
 
 //Repeat round 5 times
 function game() {
     for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice();
         playRound(playerSelection, computerSelection);
-        console.log('You played ' + playerSelection);
-        console.log('The computer played ' + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+        
     }
+
 }
+console.log('You played ' + playerSelection);
+console.log('The computer played ' + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
 
 
 
